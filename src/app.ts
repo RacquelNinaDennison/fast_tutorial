efc3d5a62d453ce9f2f87f1c491660ce1157c051
Racquel Nina Dennison <racquelnina@gmail.com>
2023-04-04 13:09:53 +0200

    Added the required modules and started the server. Tested the server through postman

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+
+const server = Fastify();
+
+server.get("/heathcheck", async function () {
+  return { status: "OK" };
+});
+
+async function main() {
+  try {
+    await server.listen(3000, "0.0.0.0");
+    console.log("Server ready at http://localhost:3000");
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+main();
